e82794a5def44d94e98f45153085051cee2cebe7
konstantine2121 <kach89kach89@gmail.com>
2026-05-19 22:54:30 +0300

    Add DI for Application and Infrastructure layers

--- working/content/Capi/Capi.API/Program.cs
+++ working/content/Capi/Capi.API/Program.cs
@@ -1,8 +1,13 @@
 using Capi.API;
+using Capi.Application;
+using Capi.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApiServices(builder.Configuration);
+builder.Services
+    .AddApiServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
